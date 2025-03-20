24b9075001fd948f3852b8558c249477fa16d55d
Griffin Keeler <144262024+griffinkeeler@users.noreply.github.com>
2025-03-19 21:37:29 -0500

    Update random_password.py
    Added more verbs to the verbs list. A for loop was added that counts the number of times a verb appears in the list.

--- random_password.py
+++ random_password.py
@@ -7,8 +7,18 @@ import random
 # A list of verbs.
 verbs = [
     'running', 'walking', 'jogging', 'flying',
-    'crying', 'yelling', 'lying', 'dancing'
+    'crying', 'yelling', 'lying', 'dancing',
+    'writing', 'thinking', 'talking', 'singing',
+    'singing', 'singing'
     ]
+
+
+# Counts the number of times a verb appears in the list. 
+# TODO: Turn into function. 
+for i in range(0 ,len(verbs)):
+    print(verbs[i])
+    verb_num = verbs.count(verbs[i])
+    print(verb_num)
 
 # A list of nouns.
 nouns = [
@@ -23,8 +33,11 @@ random_verb = random.randint(0, (len(verbs) - 1))
 # Generates a random noun from the nouns list.
 random_noun = random.randint(0, (len(nouns) - 1))
 
+# Generates a random number 10-100
+random_number = random.randint(10, 100)
+
 # A concatenation of verbs and nouns creates a random password.
-random_password = verbs[random_verb] + '_' + nouns[random_noun]
+random_password = verbs[random_verb] + '_' + nouns[random_noun] + str(random_number)
 
 
 print(random_password)
